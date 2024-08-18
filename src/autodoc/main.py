123f134ddd8eaabfd32d103d39adeaff473d005f
Tesla2000 <fratajczak124@gmail.com>
2024-08-18 13:05:53 +0200

    Updated main parametrized file extension

--- src/autodoc/main.py
+++ src/autodoc/main.py
@@ -8,12 +8,12 @@ from .config import parse_arguments
 from .transform.modify_file import modify_file
 
 
-def main():
+def main(file_extension: str = ".py"):
     args = parse_arguments(Config)
     config = create_config_with_args(Config, args)
     fails = 0
     paths = map(Path, config.filenames)
-    for filepath in filter(lambda path: path.suffix == ".py", paths):
+    for filepath in filter(lambda path: path.suffix == file_extension, paths):
         fails |= modify_file(
             filepath,
             config=config,
