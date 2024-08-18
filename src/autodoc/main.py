8c33d997cfd6e13d51321c8f2074786a45765b1b
Tesla2000 <fratajczak124@gmail.com>
2024-08-18 10:48:17 +0200

    Debugging

--- src/autodoc/main.py
+++ src/autodoc/main.py
@@ -12,7 +12,7 @@ def main():
     args = parse_arguments(Config)
     config = create_config_with_args(Config, args)
     fails = 0
-    print(config.filenames)
+    print("Filenames:", config.filenames)
     for filename in map(Path, config.filenames):
         if filename.suffix != ".py":
             continue
