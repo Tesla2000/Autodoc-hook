c3be778a350e059509b64a4bf8c4ae9d42cd56e6
Tesla2000 <fratajczak124@gmail.com>
2024-08-18 13:35:55 +0200

    Checking of main gets documented. It does

--- src/autodoc/main.py
+++ src/autodoc/main.py
@@ -9,9 +9,15 @@ from .transform.modify_file import modify_file
 
 
 def main():
+    """
+    This function processes a list of Python files based on configuration
+    parameters, modifying each file and returning a flag indicating any
+    failures encountered. It iterates through the files, applying modifications
+    defined by the configuration and reporting any errors encountered.
+    :return: Number of files modified that failed.
+    """
     args = parse_arguments(Config)
     config = create_config_with_args(Config, args)
-    # fail = 0
     fail = 0
     paths = map(Path, config.filenames)
     for filepath in filter(lambda path: path.suffix == ".py", paths):
